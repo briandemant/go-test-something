375a2a7238ad963d6455f8063abf824d048eee60
Brian Demant <brian.demant@gmail.com>
2021-09-12 19:56:33 +0200

    added read file test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAdd2(t *testing.T) {
 	var examples = []struct {
---
+++ readfile_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestReadFile(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/data.txt")
+	if err != nil {
+		t.Fatal("can't read test file")
+	}
+
+	if string(data) != "Hello World" {
+		t.Fatalf("content not as expected (expected %v got %v)", "Hello World", string(data))
+	}
+}
