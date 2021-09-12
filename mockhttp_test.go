77e532f41f5b788f0046dad9fcad143c10867674
Brian Demant <brian.demant@gmail.com>
2021-09-12 20:08:08 +0200

    mocked a https request

---
+++ mockhttp_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHttpRequest(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "status:ok")
+	}
+	req := httptest.NewRequest("GET", "https://briandemant.dk/status", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if 200 != resp.StatusCode {
+		t.Fatal("why did you fail!")
+	}
+	if string(body) != "status:ok" {
+		t.Fatalf("expected 'status:ok' got %v", string(body))
+	}
+}
